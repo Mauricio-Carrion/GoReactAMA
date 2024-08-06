e4d00b48e6c4c5f5ee065a3da26bae09c0de5d11
Mauricio Carrion <mauricio.sjn@gmail.com>
2024-08-06 18:55:13 -0300

    feat: adicionando rotas

--- internal/api/api.go
+++ internal/api/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Mauricio-Carrion/GoReactAMA/internal/store/pgstore"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type apiHandler struct {
@@ -23,8 +24,56 @@ func NewHandler(queries *pgstore.Queries) http.Handler {
 	}
 
 	router:= chi.NewRouter()
+	router.Use(
+		middleware.RequestID, 
+		middleware.Recoverer, 
+		middleware.Logger,
+	)
+
+	router.Route("/api", func(r chi.Router) {
+		router.Route("/rooms", func(r chi.Router) {
+			router.Post("/", api.handleCreateRoom)
+			router.Get("/", api.handleGetRooms)
+
+			router.Route("/{roomId}/messages", func(r chi.Router) {
+				router.Post("/", api.handleCreateRoomMessage)
+				router.Get("/", api.handleGetRoomMessages)
+
+				router.Route("/{messageId}", func(r chi.Router) {
+					router.Get("/", api.handleGetRoomMessage)
+					router.Patch("/react", api.handleReactMessage)
+					router.Delete("/react", api.handleDeleteReactMessage)
+					router.Patch("/answer", api.handleMarkAnsweredMessage)
+				})
+			})
+		})
+	})
 
 	api.router = router
 
 	return api
+}
+
+func (handler apiHandler) handleCreateRoom(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleGetRooms(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleCreateRoomMessage(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleGetRoomMessages(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleGetRoomMessage(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleReactMessage(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleDeleteReactMessage(writer http.ResponseWriter, request *http.Request) {
+}
+
+func (handler apiHandler) handleMarkAnsweredMessage(writer http.ResponseWriter, request *http.Request) {
 }
